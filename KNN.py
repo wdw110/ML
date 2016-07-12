0a9aa25f0a6263ff9faaa9529e7291dd0ec86d9d
wdw110 <wudw110@gmail.com>
2016-07-12 22:55:52 +0800

    add if main

--- KNN.py
+++ KNN.py
@@ -14,7 +14,7 @@ class KNN(object):
 
 	def save(self,fname):
 		f = open(fname,'w')
-		for i in reslut:
+		for i in self.reslut:
 			ss = '\t'.join('\t')
 			f.write(ss + '\n')
 		f.close()
@@ -25,33 +25,43 @@ class KNN(object):
 		for i in f.readlines():
 			arr = i.strip().split('\t')
 			tmp.append(arr)
-		if tp=='train': train_data = tmp
-		elif tp=='test': test_data = tmp
+		if tp=='train': self.train_data = tmp
+		elif tp=='test': self.test_data = tmp
 
 	def classify(self,k=3):
-		d1 = np.array(train_data)
-		d2 = np.array(test_data)
+		d1 = np.array(self.train_data)
+		d2 = np.array(self.test_data)
 		labels = np.array(d1[:,-1])
 		dd = d1[:,0:-1]
-		for i in d2.shape[0]:
-			tmp = d2[i]
-			obj_k = dict([(i,100000) for i in range(k)])
-			for j in d1.shape[0]:
-				distance = Euclidean_distance(d2[i][0:-1],d1[j])
+		for i in range(d2.shape[0]):
+			tmp = list(d2[i])
+			obj_k = dict([(i,100000.0) for i in range(k)])
+			for j in range(d1.shape[0]):
+				distance = Euclidean_distance(d2[i][0:-1],d1[j][0:-1])
 				arr_k = sorted(obj_k.items(), lambda x, y: cmp(x[1], y[1]))
 				if distance<arr_k[-1][1]:
-					del obj_k[arr_k[-1][0]]
 					obj_k[j] = distance
+					if len(obj_k)>k:
+						del obj_k[arr_k[-1][0]]
 			arr_labels = list(labels[obj_k.keys()])
 			label = max(arr_labels, key=arr_labels.count)#获取数组中次数最多的元素
 			tmp.append(label)
-			reslut = tmp
+			print tmp
+			self.reslut.append(tmp)
 
 	def autoNorm(self):
 		pass
 
-	def Euclidean_distance(self,a,b):
-		if len(a)==len(b):
-			return math.sqrt(sum([(a[i]-b[i])**2 for i in range(len(a))]))
-		else:
-			print '两向量的长度不一致！'
+def Euclidean_distance(a,b):
+	if len(a)==len(b):
+		return math.sqrt(sum([(float(a[i])-float(b[i]))**2 for i in range(len(a))]))
+	else:
+		print '两向量的长度不一致！'
+
+
+if __name__ == '__main__':
+	knn = KNN()
+	knn.load('knn_train.txt','train')
+	knn.load('knn_test.txt','test')
+	knn.classify(k=7)
+	#print knn.reslut
