e5c891c962f44e27e42040cb78301fb6608a8bd2
wdw110 <wudw110@gmail.com>
2016-07-28 22:47:04 +0800

    add findNN function and computeDist function

--- kdtree.py
+++ kdtree.py
@@ -1,5 +1,6 @@
 #encoding=utf-8
 
+import math
 import numpy as np
 
 data = [(2,3),(5,4),(9,6),(4,7),(8,1),(7,2)]
@@ -64,6 +65,59 @@ def computeVariance(arrayList):
 	#D[x] = E[x^2] - (E[x])^2
 	variance = sum2 / LEN - mean**2
 	return variance
+
+def findNN(root, query):
+    """
+    root:KDTree的树根
+    query:查询点
+    return:返回距离data最近的点NN，同时返回最短距离min_dist
+    """
+    #初始化为root的节点
+    NN = root.point
+    min_dist = computeDist(query, NN)
+    nodeList = []
+    temp_root = root
+    ##二分查找建立路径
+    while temp_root:
+        nodeList.append(temp_root)
+        dd = computeDist(query, temp_root.point)
+        if min_dist > dd:
+            NN = temp_root.point
+            min_dist = dd
+        #当前节点的划分域
+        ss = temp_root.split
+        if query[ss] <= temp_root.point[ss]:
+            temp_root = temp_root.left
+        else:
+            temp_root = temp_root.right
+    ##回溯查找
+    while nodeList:
+        #使用list模拟栈，后进先出
+        ss = back_point.split
+        print 'back.point = ', back_point.point
+        ##判断是否需要进入父节点的子空间进行搜索
+        if abs(query[ss] - back_point.point[ss]) < min_dist:
+            temp_root = back_point.right
+        else:
+            temp_root = back_point.left
+
+        if temp_root:
+            nodeList.append(temp_root)
+            curDist = computeDist(query, temp_root.point)
+            if min_dist > curDist:
+                min_dist = curDist
+                NN = temp_root.point
+    return NN, min_dist
+
+def computeDist(pt1, pt2):
+    """
+    计算两个数据点的距离
+    return:pt1和pt2之间的距离
+    """
+    sum1 = 0.0
+    for i in range(len(pt1)):
+        sum1 += (pt1[i] - pt2[i])**2
+    return math.sqrt(sum1)
 
 
 
