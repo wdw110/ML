6dd30a165a39cf6e2e0eacb2e3467951a2e6eaaf
wdw110 <wudw110@gmail.com>
2016-08-01 21:54:13 +0800

    add localWords function

--- bayes2.py
+++ bayes2.py
@@ -114,6 +114,38 @@ def calcMostFreq(vocabList,fullText):
 def localWords(feed1,feed0):
 	import feedparser
 	docList = []; classList = []; fullText = []
+	minLen = min(len(feed1['entries']),len(feed0['entries']))
+	for i in range(minLen):
+		wordList = textParse(feed1['tnries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(feed0['tnries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabLIst(docList)
+	top30Words = calcMostFreq(vocabList, fullText)
+	for pairW in top30Words:
+		if pairW[0] in vocabList: vocabList.remove(pairW[0])
+	trainingSet = range(2*mnLen); testSet = []
+	for i in range(20):
+		randIndex = int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat = []; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V,p1V,pSpam = trainNB0(array(tainMat),array(trainClasses))
+	errorCount = 0
+	for docIndex in testSet:
+		wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+			errorCount += 1
+	print 'the error rate is: ',float(errorCount)/len(testSet)
+	return vocabList,p0V,p1V
+
 
 
 
