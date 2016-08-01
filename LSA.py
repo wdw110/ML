12f66ab314c06f6c218c87aaed8db4ee860dca79
wdw110 <wudw110@gmail.com>
2016-08-01 16:22:25 +0800

    add part2 and part3

--- LSA.py
+++ LSA.py
@@ -1,7 +1,8 @@
 #encoding=utf-8
 
-from numpy import zeros
+import numpy
 from scipy.linalg import svd
+import math
 
 title = [
 "The Neatest Little Guide to Stock Market Investing",   
@@ -15,7 +16,7 @@ title = [
 "Rich Dad's Advisors: The ABC's of Real Estate Investing: The Secrets of Finding Hidden Profits Most Investors Miss"   
 ]
 
-stopwords = ['add', 'edition','for','in','little','of','the','to']
+stopwords = ['and', 'edition','for','in','little','of','the','to']
 ignorechars = ",:'!"  
 
 class LSA(object):
@@ -38,10 +39,11 @@ class LSA(object):
 				self.wdict[w] = [self.dcount]
 		self.dcount += 1
 
+	#part1:创建计数矩阵
 	def build(self):
 		self.keys = [k for k in self.wdict.keys() if len(self.wdict[k]) > 1]
 		self.keys.sort()
-		self.A = zeros([len(self.keys), self.dcount])
+		self.A = numpy.zeros([len(self.keys), self.dcount])
 		for i,k in enumerate(self.keys):
 			for d in self.wdict[k]:
 				self.A[i,d] += 1
@@ -49,6 +51,18 @@ class LSA(object):
 	def printA(self):
 		print self.A
 
+	#part2: 计数TFIDF代替简单计数
+	def TFIDF(self):
+		WordsPerDoc = numpy.sum(self.A, axis=0)
+		DocsPerWord = numpy.sum(numpy.asarray(self.A>0,'i'), axis=1)
+		rows, cols = self.A.shape
+		for i in range(rows):
+			for j in range(cols):
+				self.A[i,j] = (self.A[i,j]/WordsPerDoc[j]) * math.log(float(cols)/DocsPerWord[i])
+
+	#part3: 使用奇异值分解
+	def calc(self):
+		self.U,self.S,self.Vt = svd(self.A)
 
 if __name__ == '__main__':
 	mylsa = LSA(stopwords, ignorechars)
@@ -57,5 +71,7 @@ if __name__ == '__main__':
 		mylsa.parse(t)
 
 	mylsa.build()
-	mylsa.printA()
-	
+	#mylsa.printA()
+	mylsa.TFIDF()
+#	mylsa.printA()
+	mylsa.calc()
