bafac7c3de0fe16f2cc8f0264a7a563f928b6b3c
wdw110 <wudw110@gmail.com>
2016-08-05 23:45:06 +0800

    add function chooseBestFeatureToSplit

--- decision_tree.py
+++ decision_tree.py
@@ -30,7 +30,23 @@ def splitDataSet(dataSet, axis, value):
 			retDataSet.append(reduceFeatVec)
 	return retDataSet
 
-
+def chooseBestFeatureToSplit(dataSet):
+	numFeatures = len(dataSet[0] - 1)
+	baseEntropy = calcShannonEnt(dataSet)
+	bestInfoGain = 0.0; bestFeature = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataSet]
+		uniqueVals = set(featList)
+		newEntropy = 0.0
+	for value in uniqueVals:
+		subDataSet = splitDataSet(dataSet, i, value)
+		prob = len(subDataSet)/float(len(dataSet))
+		newEntropy += prob * calcShannonEnt(subDataSet)
+	infoGain = baseEntropy - newEntropy
+	if (infoGain > bestInfoGain):
+		bestInfoGain = infoGain
+		bestFeature = i 
+	return bestFeature
 
 
 
