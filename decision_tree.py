7e4ac5065a97cd3936b3a87c82f0fa7d0fc50241
wdw110 <wudw110@gmail.com>
2016-08-06 23:21:09 +0800

    add a function createTree

--- decision_tree.py
+++ decision_tree.py
@@ -63,17 +63,17 @@ def createTree(dataSet, labels):
 	if len(dataSet[0]) == 1:
 		return majorityCnt(classList)
 	bestFeat = chooseBestFeatureToSplit(dataSet)
+	bestFeatLabel = labels[bestFeat]
+	myTree = {bestFeatLabel:{}}
+	del(labels[bestFeat])
+	featValues = [example[bestFeat] for example in dataSet]
+	uniqueVals = set(featValues)
+	for value in uniqueVals:
+		subLabels = labels[:]
+		myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+	return myTree
 
-
-
-
-
-
-
-
-
-
-
+	
 
 
 
