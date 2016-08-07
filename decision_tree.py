7cc9a2d9403bdfa7ea72faa0a7688ab5af2d3700
wdw110 <wudw110@gmail.com>
2016-08-07 16:20:16 +0800

    使用文本注解绘制树节点

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,7 @@
 #encoding=utf-8
 
 from math import log
+import matplotlib.pyplot as plt 
 
 def calcShannonEnt(dataSet):
 	numEntries = len(dataSet)
@@ -73,6 +74,21 @@ def createTree(dataSet, labels):
 		myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
 	return myTree
 
+#使用文本注解绘制树节点
+decisionNode = dict(boxstyle='sawtooth',fc='0.8')
+leafNode = dict(boxstyle='round4',fc='0.8')
+arrow_args = dict(arrowstyle='<-')
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+	createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', xytext=centerPt, textcoords='axes fracton', va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
+
+def createPlot():
+	fig = plt.figure(1, faceolor='white')
+	fig.clf()
+	createPlot.ax1 = plt.subplot(111, frameon=False)
+	plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+	plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+	plt.show()
 	
 
 
