c933753cbdb51f953147abc909a3ac40bcd9bf01
wdw110 <wudw110@gmail.com>
2016-08-08 09:46:09 +0800

    add some functions

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,6 @@
 #encoding=utf-8
 
+import pickle
 from math import log
 import matplotlib.pyplot as plt 
 
@@ -89,22 +90,87 @@ def createPlot():
 	plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
 	plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
 	plt.show()
-	
 
+#获取叶节点的数目和树的层数
+def getNumLeafs(myTree):
+	numLeafs = 0
+	firstStr = myTree.keys()[0]
+	secondDict = myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			numLeafs += getNumLeafs(secondDict[key])
+		else: numLeafs += 1
+	return numLeafs
 
+def getTreeDepth(myTree):
+	maxDepth = 0
+	firstStr = myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			thisDepth = 1 + getTreeDepth(secondDict[key])
+		else: thisDepth = 1
+		if thisDepth > maxDepth: maxDepth = thisDepth
+	return maxDepth
 
+def retrieveTree(i):
+	listOfTrees = [{'no surfacing':{0:'no', 1:{'flippers':{0:'no', 1:'yes'}}}},{'no surfacing':{0:'no', 1:{'flippers':{0:{'head':{0:'no', 1:'yes'}}, 1:'no'}}}}]
+	return listOfTrees[i]
 
+# plotTree函数
+def plotMidText(cntrPt, parentPt, txtString):
+	xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+	yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+	createPlot.ax1.text(xMid, yMid, txtString)
 
+def plotTree(myTree, parentPt, nodeType):
+	numLeafs = getNumLeafs(myTree)
+	depth = getTreeDepth(myTree)
+	firstStr = myTree.keys()[0]
+	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+	plotMidText(cntrPt, parentPt, nodeTxt)
+	plotNode(firstStr, cntrPt, parentPt, decisionNode)
+	secondDict = myTree[firstStr]
+	plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			plotTree(secondDict[key],cntrPt,str(key))
+		else:
+			plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+			plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+			plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+		plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
 
+def createPlot(inTree):
+	fig = plt.figure(1, faceolor='white')
+	fig.clf()
+	axprops = dict(xticks=[], yticks=[])
+	createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+	plotTree.totalW = float(getNumLeafs(inTree))
+	plotTree.totalD = float(getTreeDepth(inTree))
+	plotTree.xOff = -0.5/plotTree.totalW
+	plotTree.yOff = 1.0
+	plotTree(inTree, (0.5,1.0), '')
+	plt.show()
 
+#使用决策树的分类函数
+def classify(inputTree,featLabels,testVec):
+	firstStr = inputTree.keys()[0]
+	secondDict = inputTree[firstStr]
+	featIndex = featLabels.index(firstStr)
+	for key in secondDict.keys():
+		if testVec[featIndex] == key:
+			if type(secondDict[key]).__name__=='dict':
+				classLabel = classify(secondDict[key],featLabels,testVec)
+			else: classLabel = secondDict[key]
+	return classLabel
 
+#使用pickle模块存储决策树
+def storeTree(inputTree, filename):
+	fw = open(filename, 'w')
+	pickle.dump(inputTree, fw)
+	fw.close()
 
-
-
-
-
-
-
-
-
+def grabTree(filename):
+	fr = open(filename)
+	return pickle.load(fr)
 
