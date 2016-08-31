132e098b37c55f020ab34da0213474be1f30c864
wdw110 <wudw110@gmail.com>
2016-09-01 00:55:41 +0800

    finish adaboost.py script

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -34,7 +34,7 @@ def buildStump(dataArr,classLabels,D):
 				errArr = np.mat(np.ones((m,1)))
 				errArr[predictedVals == labelMat] = 0
 				weightedError = D.T*errArr
-				print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" %(i, threshVal, inequal, weightedError)
+				#print "split: dim %d, thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" %(i, threshVal, inequal, weightedError)
 				if weightedError < minError:
 					minError = weightedError
 					bestClasEst = predictedVals.copy()
@@ -42,38 +42,33 @@ def buildStump(dataArr,classLabels,D):
 					bestStump['thresh'] = threshVal
 					bestStump['ineq'] = inequal
 	return bestStump,minError,bestClasEst
-	
 
-print 1
-print 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+	weakClassArr = []
+	m = np.shape(dataArr)[0]
+	D = np.mat(np.ones((m,1))/m)
+	aggClassEst = np.mat(np.zeros((m,1)))
+	for i in range(numIt):
+		bestStump,error,classEst = buildStump(dataArr,classLabels,D)
+		print 'D:',D.T
+		alpha = float(0.5*np.log((1-error)/max(error,1e-16)))
+		bestStump['alpha'] = alpha
+		weakClassArr.append(bestStump)
+		print 'classEst: ',classEst.T 
+		expon = np.multiply(-1*alpha*np.mat(classLabels).T,classEst)
+		D = np.multiply(D,np.exp(expon))
+		D = D/D.sum()
+		aggClassEst += alpha*classEst
+		print 'aggClassEst: ',aggClassEst.T 
+		aggErrors = np.multiply(np.sign(aggClassEst)!=np.mat(classLabels).T,np.ones((m,1)))
+		errorRate = aggErrors.sum()/m
+		print 'total error: ',errorRate
+		if errorRate == 0.0: break
+	return weakClassArr
 
 
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	dataMat, classLabels = loadSimpData()
+	#D = np.mat(np.ones((5,1))/5)
+	#print buildStump(dataMat,classLabels,D)
+	classifierArray = adaBoostTrainDS(dataMat,classLabels,9)
