588ead9bbb51a7d8a02441d75a7fcb3aaa42024a
wdw110 <wudw110@gmail.com>
2016-09-04 00:08:52 +0800

    add class keyword

--- keyword_extract/text_rank.py
+++ keyword_extract/text_rank.py
@@ -4,3 +4,18 @@ import re
 import jieba
 import numpy as np 
 
+class keyword(object):
+	"""docstring for keyword"""
+	def __init__(self, arg):
+		self.docs = []
+		self.arg = arg
+
+	def read_file(filename):
+		with open(filename,'r') as f:
+			for i in f.readlines():
+				if not i.split():
+					continue
+				self.docs.append(i)
+		
+
+
